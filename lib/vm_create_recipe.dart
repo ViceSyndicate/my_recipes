4255c6a227a74566db4cf6afe561fea08b4de651
Victor S <victor.skarerverket@gmail.com>
2023-01-22 12:56:19 +0100

    Working on recipe form

--- lib/vm_create_recipe.dart
+++ lib/vm_create_recipe.dart
@@ -2,26 +2,109 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:my_recipes/model_recipe.dart';
 
-class RecipeFormPage extends StatefulWidget {
-  @override
-  _RecipeFormPageState createState() => _RecipeFormPageState();
-}
-
-class _RecipeFormPageState extends State<RecipeFormPage> {
+class RecipeFormPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: const Text('Navigate to a new screen on Button click'),
+          title: const Text('Back to Recipes'),
           backgroundColor: Colors.blueAccent),
-      body: Center(
-        child: TextButton(
+      body: const MyCustomForm(),
+      floatingActionButton: FloatingActionButton(
           onPressed: () {
-            Navigator.pop(context);
+            // create_recipe page
+            Navigator.pop(
+              context,
+            );
           },
-          child: Text('GO TO HOME'),
-        ),
+          backgroundColor: Colors.lightBlue,
+          child: const Icon(Icons.save)),
+    );
+  }
+}
+
+// Create a Form widget.
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+// Create a corresponding State class.
+// This class holds data related to the form.
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8),
+          ),
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'Recipe Name'),
+            // The validator receives the text that the user has entered.
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'Recipe Notes'),
+            // The validator receives the text that the user has entered.
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+        ],
       ),
     );
   }
 }
+
+
+//Navigator.pop(context);
+
+/*
+return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter a search term',
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter your username',
+            ),
+          ),
+        ),
+      ],
+    );
+*/
